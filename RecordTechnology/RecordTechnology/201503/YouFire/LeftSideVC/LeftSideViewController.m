a26b6cc4b7f279653c3b932e3a8ec506a4d8228e
ArthurWang <yinanwang1@hengtiansoft.com>
2015-03-24 15:56:47 +0800

    Complete Left View.

--- RecordTechnology/RecordTechnology/201503/YouFire/LeftSideVC/LeftSideViewController.m
+++ RecordTechnology/RecordTechnology/201503/YouFire/LeftSideVC/LeftSideViewController.m
@@ -8,7 +8,12 @@
 
 #import "LeftSideViewController.h"
 
-@interface LeftSideViewController ()
+@interface LeftSideViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UILabel *usrNameLabel;
+@property (weak, nonatomic) IBOutlet UIButton *headPortraitBtn;
+@property (weak, nonatomic) IBOutlet UITableView *functionTableView;
+
+@property (nonatomic, strong) NSArray *dataSource;
 
 @end
 
@@ -17,11 +22,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    _dataSource = @[@"意见反馈", @"关于我们", @"常见问题", @"清除缓存", @"修改密码", @"求好评", @"退出"];
+    
+    self.headPortraitBtn.layer.cornerRadius = 50.0f;
+    [self.headPortraitBtn setBackgroundImage:[UIImage imageNamed:@"haizeiwang"] forState:UIControlStateNormal];
+    [self.headPortraitBtn setTitle:@"" forState:UIControlStateNormal];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
 }
 
 /*
@@ -33,5 +50,31 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dataSource count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifierStr = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifierStr];
+    
+    if ( nil == cell )
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifierStr];
+        [cell setBackgroundColor:[UIColor clearColor]];
+        cell.textLabel.textColor = [UIColor orangeColor];
+    }
+    
+    cell.textLabel.text = [self.dataSource objectAtIndex:indexPath.row];
+    
+    return cell;
+}
 
 @end
