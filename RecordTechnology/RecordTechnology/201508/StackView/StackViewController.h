59f46730d2e2028dc051faafc447cc8dd7e3bffd
阿南 <wangyn@59store.com>
2015-08-21 16:47:54 +0800

    Add Stack view Demo.

---
+++ RecordTechnology/RecordTechnology/201508/StackView/StackViewController.h
@@ -0,0 +1,13 @@
+//
+//  StackViewController.h
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/8/19.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StackViewController : UIViewController
+
+@end
---
+++ RecordTechnology/RecordTechnology/201508/StackView/StackViewController.m
@@ -0,0 +1,38 @@
+//
+//  StackViewController.m
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/8/19.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import "StackViewController.h"
+
+@interface StackViewController ()
+
+@end
+
+@implementation StackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
