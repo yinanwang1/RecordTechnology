a7313bb74c0a4808027e7949bf2dda6c5a776776
阿南 <wangyn@59store.com>
2015-10-06 09:49:05 +0800

    UIDynamicAnimator demo.

---
+++ RecordTechnology/RecordTechnology/201510/BehaviorViewController.h
@@ -0,0 +1,13 @@
+//
+//  BehaviorViewController.h
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/10/5.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BehaviorViewController : UIViewController
+
+@end
