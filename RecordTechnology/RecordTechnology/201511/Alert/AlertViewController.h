c0e84992dfd0ac2d854bac2e2cc34c3f52ebe3b5
阿南 <wangyn@59store.com>
2015-11-18 16:27:27 +0800

    Add a Alert Demo.

---
+++ RecordTechnology/RecordTechnology/201511/Alert/AlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlertViewController.h
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/11/18.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertViewController : UIViewController
+
+@end
