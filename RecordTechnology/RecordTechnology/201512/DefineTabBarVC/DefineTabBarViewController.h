359446e9a77bf62ea193cb1580041389f033e354
阿南 <wangyn@59store.com>
2015-12-30 18:41:37 +0800

    Tab bar.

---
+++ RecordTechnology/RecordTechnology/201512/DefineTabBarVC/DefineTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  DefineTabBarViewController.h
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/12/30.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DefineTabBarViewController : UITabBarController
+
+@end
---
+++ RecordTechnology/RecordTechnology/201512/DefineTabBarVC/FirstViewController.m
@@ -0,0 +1,40 @@
+//
+//  FirstViewController.m
+//  RecordTechnology
+//
+//  Created by ArthurWang on 15/12/30.
+//  Copyright © 2015年 InSigma HengTian Softwar Ltd. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tabBarItem.title = @"123";
+    self.tabBarItem.badgeValue = @"";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
